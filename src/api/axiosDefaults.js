f0f34111bef3ee22cc7c385838365aaf12473f74
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2025-01-04 16:14:53 +0000

    update to moments code

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+// Set global defaults for axios
 axios.defaults.baseURL = 'https://dfri-app-dc6e57a8e2dd.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
+
+// Create and export custom axios instances
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
